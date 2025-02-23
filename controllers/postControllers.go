0623c59b32edfd7711bbf9c752a6840eea8e89b6
truptisatsangi <truptisatsangi18@gmail.com>
2025-02-23 23:28:32 +0530

    feat: add patch and delete API

--- controllers/postControllers.go
+++ controllers/postControllers.go
@@ -13,17 +13,17 @@ func PostsCreate(ctx *gin.Context) {
 
 	var body struct {
 		Title string `json: "title" binding: "required" `
-		Body string `json: "body" binding: required`
+		Body  string `json: "body" binding: required`
 	}
 	ctx.Bind(&body)
 
 	// Get data off the request body creat a post and return it
 
-	post:= models.Post{Title: body.Title, Body:body.Body}
-	result:=initializers.DB.Create(&post)
+	post := models.Post{Title: body.Title, Body: body.Body}
+	result := initializers.DB.Create(&post)
 
-	if(result.Error != nil ){
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message" : "Error creating post"})
+	if result.Error != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Error creating post"})
 		return
 	}
 	ctx.JSON(http.StatusOK, gin.H{"message": "Post created successfully", "post": post})
@@ -34,4 +34,53 @@ func PostFindAll(ctx *gin.Context) {
 	var posts []models.Post
 	initializers.DB.Find(&posts)
 	ctx.JSON(http.StatusOK, gin.H{"message": "All posts", "posts": posts})
+}
+
+func PostFindOne(ctx *gin.Context) {
+	var post models.Post
+
+	id := ctx.Param("id")
+
+	if err := initializers.DB.First(&post, id).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Post not found"})
+		return
+	}
+	initializers.DB.First(&post, id)
+	ctx.JSON(http.StatusOK, gin.H{"message": "Post found", "post": post})
+}
+
+func PostUpdate(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var input struct {
+		Title string `json:"title"`
+		Body  string `json:"body"`
+	}
+
+	ctx.Bind(&input)
+	var post models.Post
+	if err := initializers.DB.First(&post, id).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Post not found"})
+		return
+	}
+
+	if err := initializers.DB.Model(&post).Updates(&models.Post{Title: input.Title, Body: input.Body}).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update post"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Post updated", "post": post})
+}
+
+func PostDelete(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	if (initializers.DB.First(&models.Post{}, id).RowsAffected == 0) {
+		ctx.JSON(404, gin.H{"message": "Post not found"})
+		return
+	}
+
+	initializers.DB.Delete(&models.Post{}, id)
+
+	ctx.JSON(200, gin.H{"message": "Post deleted successfully"})
 }
--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"go-gin/controllers"
 	"go-gin/initializers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
-	initializers.ConnectToDB();
+	initializers.ConnectToDB()
 }
 
 func main() {
-	r := gin.Default();
+	r := gin.Default()
 	r.POST("/posts", controllers.PostsCreate)
 	r.GET("/posts", controllers.PostFindAll)
-	r.Run();
+	r.GET("/posts/:id", controllers.PostFindOne)
+	r.PATCH("/post/:id", controllers.PostUpdate)
+	r.DELETE("/post/:id", controllers.PostDelete)
+	r.Run()
 }
